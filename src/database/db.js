c98238e497b06e6cd23a2a6485f8d4f2446ec473
jacrifla <jaquelini_cris@hotmail.com>
2025-04-25 00:30:45 -0300

    teste

--- src/database/db.js
+++ src/database/db.js
@@ -1,12 +1,13 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
+require('dotenv').config();
+
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-});
-
-pool.on('connect', () => {
-    console.log('🚀 Connected to PostgreSQL');
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: 'postgres',
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 module.exports = pool;

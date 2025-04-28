5be7af31d2b466feede57eb13e8b032b2dc7a919
jacrifla <jaquelini_cris@hotmail.com>
2025-04-28 17:03:27 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ const app = express();
 
 // Configuração do CORS
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://procura-filmes.netlify.app', 'https://server-filmes-production.up.railway.app'],
+  origin: [
+    'http://localhost:5173', 
+    'https://procura-filmes.netlify.app', 
+    'https://server-filmes-production.up.railway.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
@@ -25,6 +29,12 @@ app.use('/watchlist', require('./routes/watchlistRoutes'));
 // Rota inicial
 app.get('/', (req, res) => {
   res.send('API is running 🎬');
+});
+
+// Middleware para tratar erros
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Algo deu errado no servidor! 🚨' });
 });
 
 module.exports = app;

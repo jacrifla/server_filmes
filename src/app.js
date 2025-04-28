cf01a0514c9b4119f86ec4576098bacad66ed62c
jacrifla <jaquelini_cris@hotmail.com>
2025-04-28 16:07:05 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -2,15 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+// Configuração do CORS
+const corsOptions = {
+  origin: ['http://localhost:4000', 'https://procura-filmes.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
+// Rotas da API
 app.use('/media', require('./routes/mediaRoutes'));
 app.use('/ratings', require('./routes/ratingRoutes'));
 app.use('/auth', require('./routes/authRoutes'));
 app.use('/user', require('./routes/userRoutes'));
 app.use('/watchlist', require('./routes/watchlistRoutes'));
 
+// Rota inicial
 app.get('/', (req, res) => {
   res.send('API is running 🎬');
 });
